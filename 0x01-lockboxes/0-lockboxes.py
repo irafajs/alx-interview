96b481af13c1255e04f2a242ce19121eed458a9c
irafajs <irafajs@gmail.com>
2024-03-12 22:14:58 +0200

    TASK ALL

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Shebang to create a py script
+"""
+
+
+def canUnlockAll(boxes):
+    """method to check and unlock the box if the
+       have a key, return true, else return false"""
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+
+    def dfs(box_index):
+        """method to search thorough each box"""
+        visited[box_index] = True
+        for key in boxes[box_index]:
+            if not visited[key]:
+                dfs(key)
+
+    dfs(0)
+
+    return all(visited)
