8b932a267baa9c1ff7c496299590c64350465079
irafajs <irafajs@gmail.com>
2024-04-11 14:18:25 +0200

    TASK00

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Shebang to create a PY script
+"""
+
+
+import sys
+
+
+def is_safe(board, row, col, N):
+    """method to check if the boad has not quen"""
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens(N):
+    """method to solve the nqueen problem"""
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+
+    def backtrack(row):
+        """backtrack method"""
+        if row == N:
+            solution = []
+            for i in range(N):
+                for j in range(N):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            solutions.append(solution)
+            return
+
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 1
+                backtrack(row + 1)
+                board[row][col] = 0
+
+    backtrack(0)
+
+    for solution in solutions:
+        print(solution)
+
+
+solve_nqueens(sys.argv[1])
